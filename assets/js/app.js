42f581384be97bde078e01f91d59b3cebe917eaa
Jesus Cordoba <jessusscj@gmail.com>
2022-10-28 12:52:25 -0500

    Se corrigen los errores del codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,28 @@
+/**
+ * Este script consulta una appi y se le pasa un nombre de usuario y esta api devuelve informacion del usuario
+ * Se muestra en pantalla el nombre de usuario su blog y su localizacion
+ * 
+ */
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');//se agrego la notacion para seleccionar una clase
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+/**
+ * 
+ * se agrego la palbra async para hacer uso del async await
+ * se cambiaron las comillas simples por backsticks para hacer uso de 
+ * template strings
+ *  
+ */
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await fetch(`${usersEndpoint}/${username}`).then(response => response.json()).catch(error => handleError(err));
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
@@ -20,3 +32,4 @@ function handleError(err) {
 }
 
 displayUser('stolinski').catch(handleError);
+
